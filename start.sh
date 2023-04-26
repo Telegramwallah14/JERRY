1636dc435a954e287f95bf3883848dfe43d49b18
Telegramwallah14 <131813732+Telegramwallah14@users.noreply.github.com>
2023-04-26 16:35:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/senapatibubai111/JERRY.git /JERRY
+  git clone https://github.com/Telegramwallah14/JERRY.git /JERRY
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO"
   git clone $UPSTREAM_REPO /JERRY
